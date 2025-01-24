45c532d7f20b74bd9913c5c419fb4f6f6d114a21
Esteban De La Fuente Rubio <esteban@delaf.cl>
2025-01-24 08:54:41 -0300

    Se agrega Str::uuid4().

--- src/Helper/Str.php
+++ src/Helper/Str.php
@@ -123,4 +123,27 @@ class Str extends IlluminateStr
 
         return $template;
     }
+
+    /**
+     * Genera un UUID versión 4 con la variante RFC 4122.
+     *
+     * @return string
+     */
+    public static function uuid4(): string
+    {
+        $data = random_bytes(16);
+
+        // Ajusta las versiones y variantes del UUID.
+        $data[6] = chr((ord($data[6]) & 0x0f) | 0x40); // Versión 4.
+        $data[8] = chr((ord($data[8]) & 0x3f) | 0x80); // Variante RFC 4122.
+
+        return sprintf(
+            '%08s-%04s-%04s-%04s-%12s',
+            bin2hex(substr($data, 0, 4)),
+            bin2hex(substr($data, 4, 2)),
+            bin2hex(substr($data, 6, 2)),
+            bin2hex(substr($data, 8, 2)),
+            bin2hex(substr($data, 10, 6))
+        );
+    }
 }
